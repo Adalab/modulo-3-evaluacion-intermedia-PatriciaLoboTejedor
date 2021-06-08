74b955806c435fefa73337d32999dda5a6e986bb
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-06-08 09:34:26 +0200

    create PokeList and Pokemon components

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,13 +1,18 @@
 import Pokemon from "./Pokemon";
 import "../stylesheets/pokelist.scss";
+/*import PropTypes from "prop-types";*/
 
-function PokeList() {
-  return (
-    <div>
-      <p>Pokelist</p>
-      <Pokemon />
-    </div>
-  );
+function PokeList(props) {
+  const pokeList = props.pokedata.map((pokeItem) => (
+    <li key={pokeItem.id}>
+      <Pokemon pokeItem={pokeItem} />
+    </li>
+  ));
+  return <ul>{pokeList}</ul>;
 }
+
+/*PokeList.propTypes = {
+  pokeList: PropTypes.arrayOf(PropTypes.object).isRequired,
+};*/
 
 export default PokeList;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,9 +1,27 @@
-function Pokemon() {
+/*import PropTypes from "prop-types";*/
+
+function Pokemon(props) {
+  const poketypes = () => {
+    return props.pokeItem.types.map((poketype, index) => {
+      return <li key={index}>{poketype}</li>;
+    });
+  };
+
   return (
-    <div>
-      <p>Pokemon</p>
-    </div>
+    <article className="pokeCard">
+      <img
+        className="pokeImage"
+        src={props.pokeItem.url}
+        alt={props.pokeItem.name}
+      />
+      <h2 className="pokeName">{props.pokeItem.name}</h2>
+      <ul className="pokeTypes">{poketypes()}</ul>
+    </article>
   );
 }
+
+/*Pokemon.propTypes = {
+  props.pokeItem.name: PropTypes.objectOf(PropTypes.string).isRequired
+};*/
 
 export default Pokemon;
